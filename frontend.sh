de0f4156973a19dc9d1f128364efd7879a3076e6
durga <dutgarajesh899@gmail.com>
2023-05-18 17:52:06 +0530

    Added print statements

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,18 @@
+ echo Installing Ngnix
  yum install nginx -y
 
+ echo Downloading web content
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
  cd /usr/share/nginx/html
+ echo Removing old content
  rm -rf *
+ echo Etracting web content
  unzip /tmp/frontend.zip
+
  mv frontend-main/static/* .
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
+ echo starting ngnix service
  systemctl enable nginx
  systemctl restart nginx
