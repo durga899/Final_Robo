af869ab887f1a6dd057cdbb1e354e2827ff5ccee
durga <dutgarajesh899@gmail.com>
2023-05-19 22:10:08 +0530

    Adding variable

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,9 @@
+ log_file=/tmp/frontend
  echo Installing Ngnix
- yum install nginx -y &>>/tmp/frontend
+ yum install nginx -y &>>log_file
  echo status = $?
  echo Downloading web content
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>log_file
  echo status = $?
 
  cd /usr/share/nginx/html
@@ -10,15 +11,14 @@
  rm -rf *
  echo status = $?
  echo Etracting web content
- unzip /tmp/frontend.zip &>>/tmp/frontend
+ unzip /tmp/frontend.zip &>>log_file
  echo status = $?
 
  mv frontend-main/static/* .
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
+ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>log_file
  echo status = $?
  echo starting ngnix service
- systemctl enable nginx &>>/tmp/frontend
+ systemctl enable nginx &>>log_file
  echo status = $?
- systemctl restart nginx &>>/tmp/frontend
+ systemctl restart nginx &>>log_file
  echo status = $?
- echo done
