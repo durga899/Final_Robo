d395b9e7afa43bc1e6086af678df82ea053246ea
durga <dutgarajesh899@gmail.com>
2023-05-19 21:51:23 +0530

    hiding output

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,23 @@
  echo Installing Ngnix
  yum install nginx -y &>>/tmp/frontend
+ echo status = $?
  echo Downloading web content
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+ echo status = $?
 
  cd /usr/share/nginx/html
  echo Removing old content
  rm -rf *
+ echo status = $?
  echo Etracting web content
  unzip /tmp/frontend.zip &>>/tmp/frontend
+ echo status = $?
 
  mv frontend-main/static/* .
  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
-
+ echo status = $?
  echo starting ngnix service
  systemctl enable nginx &>>/tmp/frontend
+ echo status = $?
  systemctl restart nginx &>>/tmp/frontend
+ echo status = $?
