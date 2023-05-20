93bc3298e0cb822e8b0b71568af0d17d51f26aa1
durga <dutgarajesh899@gmail.com>
2023-05-20 11:54:28 +0530

    Adding variable

---
+++ mongodb.sh
@@ -0,0 +1,16 @@
+echo setting =mongodb repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> /tmp/mongodb
+echo status = $?
+
+echo Installing mongodb
+yum install -y mongodb-org &>> /tmp/mongodb
+echo status = $?
+
+echo Enable mongodb
+systemctl enable mongod &>> /tmp/mongodb
+echo status = $?
+
+echo Starting mongodb
+systemctl start mongod &>> /tmp/mongodb
+echo status = $?
+
