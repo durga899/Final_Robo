280151efff01666c1d4362610fa0fc6bd1d2a17e
durga <dutgarajesh899@gmail.com>
2023-05-23 17:01:20 +0530

    Adding if statement

--- catalogue.sh
+++ catalogue.sh
@@ -2,41 +2,73 @@ log_file=/tmp/catalogue
 
 echo Setup Nodejs repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-echo status = $?
+if [ $? == 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo Installing nodejs
 yum install nodejs -y &>>${log_file}
-echo status = $?
+if [ $? == 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo Adding roboshop user
 useradd roboshop &>>${log_file}
-echo status = $?
+if [ $? == 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo Download catalogue Application code
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${log_file}
-echo status = $?
+if [ $? == 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 cd /home/roboshop
 
 echo Etract catalogue schema
 unzip /tmp/catalogue.zip &>>${log_file}
-echo status = $?
+if [ $? == 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
-mv catalogue-main catalogue
+mv catalogue-main catalogue &>>${log_file}
 cd /home/roboshop/catalogue
 
 echo Installing dependencies
 npm install &>>${log_file}
-echo status = $?
+if [ $? == 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 echo setup catalogue service
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-echo status = $?
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${log_file}
+if [ $? == 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
 systemctl daemon-reload
 systemctl enable catalogue
 
 echo Start catalogue service
 systemctl start catalogue
-echo status = $?
+if [ $? == 0 ]; then
+  echo Status = Success
+else
+  echo Status = Failure
+fi
 
